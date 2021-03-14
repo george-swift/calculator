e8ae0f0d74ab4bcbc209f58415ea4d0c7c9bfdcc
George <71434803+george-swift@users.noreply.github.com>
2021-03-14 15:03:00 +0100

    Add cached elements

---
+++ script.js
@@ -0,0 +1,4 @@
+const memory = document.querySelector('.calculator');
+const calcButtons = document.querySelector('.buttons');
+const input = document.querySelector('#expression');
+const solution = document.querySelector('#answer');
