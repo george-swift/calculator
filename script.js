908d659b912267c3d31d9ef02adbc0d200dbb4a3
George <71434803+george-swift@users.noreply.github.com>
2021-03-14 15:05:18 +0100

    Add event listener to print key values

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const memory = document.querySelector('.calculator');
 const calcButtons = document.querySelector('.buttons');
 const input = document.querySelector('#expression');
 const solution = document.querySelector('#answer');
+
+function handleEvent(e) {
+  if (!e.target.closest('button')) return;
+  let key = e.target;
+  let keyValue = key.textContent;
+  console.log(keyValue);
+}
+
+calcButtons.addEventListener('click', handleEvent);
